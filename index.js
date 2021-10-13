aa68f3d237a96286098ee2068f5d8c78686899b9
unknown <sofiabernabeicejas@gmail.com>
2021-10-12 23:24:32 -0300

    comienzo con js

---
+++ index.js
@@ -0,0 +1,26 @@
+let seccionNuevaOperacion = document.getElementById("section-nueva-operacion")
+let botonNuevaOperacion = document.getElementById("boton-nueva-operacion")
+let seccionPrincipal = document.getElementById("seccion-principal")
+
+
+
+
+
+
+
+
+
+// boton nueva operacion --- abrir seccion nueva operacion //
+
+
+botonNuevaOperacion.onclick = () => {
+    seccionNuevaOperacion.classList.add = "is-invisible"
+    seccionNuevaOperacion.classList.remove = "is-hidden"
+    seccionPrincipal.classList.add = "is-invisible"
+
+
+
+}  
+
+
+
