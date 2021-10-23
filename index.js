06b90ad7c1c744b95126b6687dde9f3d8f351ea1
unknown <sofiabernabeicejas@gmail.com>
2021-10-23 19:47:21 -0300

    seccion datos operacion, tutilo y div para modificar con JS. Boton Agregar en seccion nueva operacion funcionando

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ const botonBalance = document.getElementById("boton-balance")
 const botonCategorias = document.getElementById("boton-categorias")
 const botonOcultarFiltros = document.getElementById("boton-ocultar-filtros")
 const divFormularioFiltros = document.getElementById("div-formulario-filtros")
+const divOperacionesImagenTexto = document.getElementById("div-operaciones-imagen-texto")
+const divDatosOperacionesTitulo = document.getElementById("div-datos-operaciones-titulo")
+const botonAgregarFormularioNuevaOperacion = document.getElementById("boton-agregar-formulario-nueva-operacion")
 
+
+// div datos js para modificar
+const divDatosOperacionJS = document.getElementById("div-datos-operacion-jS")
+
+// boton "agregar" en SECCION NUEVA OPERACION 
+ 
+botonAgregarFormularioNuevaOperacion.onclick = () => {
+    divOperacionesImagenTexto.classList.add("is-hidden")
+    divDatosOperacionesTitulo.classList.remove("is-hidden")
+    seccionNuevaOperacion.classList.add("is-hidden")
+    seccionPrincipal.classList.remove("is-hidden")
+}
 
 
 // boton balance  
 
 botonBalance.onclick = () => {
+    
     seccionPrincipal.classList.remove("is-hidden")
     seccionCategoria.classList.add("is-hidden")
     seccionReporte.classList.add("is-hidden")
     seccionNuevaOperacion.classList.add("is-hidden")
+    divDatosOperacionesTitulo.classList.add("is-hidden")
+    divOperacionesImagenTexto.classList.remove("is-hidden")
 }
 
 // boton categorias 
@@ -38,6 +56,12 @@ botonReporte.onclick = () => {
     seccionNuevaOperacion.classList.add("is-hidden")
 }
 
+//boton ocultar filtros SECCION FILTRO
+
+botonOcultarFiltros.onclick = () => {
+    divFormularioFiltros.classList.toggle("is-hidden")
+}
+
 // boton nueva operacion SECCION OPERACIONES 
 
 
@@ -48,8 +72,4 @@ botonNuevaOperacion.onclick = () => {
 }  
 
 
-//boton ocultar filtros SECCION FILTRO
 
-botonOcultarFiltros.onclick = () => {
-    divFormularioFiltros.classList.toggle("is-hidden")
-}
