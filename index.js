cdc3a36b01cacfde9c2d8bb85160ac6c5d010288
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-11-05 21:06:12 -0300

    funcion agregar categorias

--- index.js
+++ index.js
@@ -1,75 +1,133 @@
-const seccionNuevaOperacion = document.getElementById("section-nueva-operacion")
-const botonNuevaOperacion = document.getElementById("boton-nueva-operacion")
-const seccionPrincipal = document.getElementById("seccion-principal")
-const seccionCategoria = document.getElementById("seccion-categoria")
-const seccionReporte = document.getElementById("seccion-reporte")
-const botonReporte = document.getElementById("boton-reporte")
-const botonBalance = document.getElementById("boton-balance")
-const botonCategorias = document.getElementById("boton-categorias")
-const botonOcultarFiltros = document.getElementById("boton-ocultar-filtros")
-const divFormularioFiltros = document.getElementById("div-formulario-filtros")
-const divOperacionesImagenTexto = document.getElementById("div-operaciones-imagen-texto")
-const divDatosOperacionesTitulo = document.getElementById("div-datos-operaciones-titulo")
-const botonAgregarFormularioNuevaOperacion = document.getElementById("boton-agregar-formulario-nueva-operacion")
-
-
+const seccionNuevaOperacion = document.getElementById(
+	"section-nueva-operacion"
+);
+const botonNuevaOperacion = document.getElementById("boton-nueva-operacion");
+const seccionPrincipal = document.getElementById("seccion-principal");
+const seccionCategoria = document.getElementById("seccion-categoria");
+const seccionReporte = document.getElementById("seccion-reporte");
+const botonReporte = document.getElementById("boton-reporte");
+const botonBalance = document.getElementById("boton-balance");
+const botonCategorias = document.getElementById("boton-categorias");
+const botonOcultarFiltros = document.getElementById("boton-ocultar-filtros");
+const divFormularioFiltros = document.getElementById("div-formulario-filtros");
+const divOperacionesImagenTexto = document.getElementById(
+	"div-operaciones-imagen-texto"
+);
+const divDatosOperacionesTitulo = document.getElementById(
+	"div-datos-operaciones-titulo"
+);
+const botonAgregarFormularioNuevaOperacion = document.getElementById(
+	"boton-agregar-formulario-nueva-operacion"
+);
+const inputAgregarCategorias = document.getElementById(
+	"input-agregar-categorias"
+);
+const botonAgregarCategorias = document.getElementById(
+	"boton-agregar-categorias"
+);
 // div datos js para modificar
-const divDatosOperacionJS = document.getElementById("div-datos-operacion-jS")
+const divDatosOperacionJS = document.getElementById("div-datos-operacion-jS");
 
-// boton "agregar" en SECCION NUEVA OPERACION 
- 
+// boton "agregar" en SECCION NUEVA OPERACION
+
 botonAgregarFormularioNuevaOperacion.onclick = () => {
-    divOperacionesImagenTexto.classList.add("is-hidden")
-    divDatosOperacionesTitulo.classList.remove("is-hidden")
-    seccionNuevaOperacion.classList.add("is-hidden")
-    seccionPrincipal.classList.remove("is-hidden")
-}
+	divOperacionesImagenTexto.classList.add("is-hidden");
+	divDatosOperacionesTitulo.classList.remove("is-hidden");
+	seccionNuevaOperacion.classList.add("is-hidden");
+	seccionPrincipal.classList.remove("is-hidden");
+};
 
-
-// boton balance  
+// boton balance
 
 botonBalance.onclick = () => {
-    
-    seccionPrincipal.classList.remove("is-hidden")
-    seccionCategoria.classList.add("is-hidden")
-    seccionReporte.classList.add("is-hidden")
-    seccionNuevaOperacion.classList.add("is-hidden")
-    divDatosOperacionesTitulo.classList.add("is-hidden")
-    divOperacionesImagenTexto.classList.remove("is-hidden")
-}
+	seccionPrincipal.classList.remove("is-hidden");
+	seccionCategoria.classList.add("is-hidden");
+	seccionReporte.classList.add("is-hidden");
+	seccionNuevaOperacion.classList.add("is-hidden");
+	divDatosOperacionesTitulo.classList.add("is-hidden");
+	divOperacionesImagenTexto.classList.remove("is-hidden");
+};
 
-// boton categorias 
+// boton categorias
 
 botonCategorias.onclick = () => {
-    seccionCategoria.classList.remove("is-hidden")
-    seccionPrincipal.classList.add("is-hidden")
-    seccionReporte.classList.add("is-hidden")
-    seccionNuevaOperacion.classList.add("is-hidden")
-}
+	seccionCategoria.classList.remove("is-hidden");
+	seccionPrincipal.classList.add("is-hidden");
+	seccionReporte.classList.add("is-hidden");
+	seccionNuevaOperacion.classList.add("is-hidden");
+};
+
+//funcion agregar categorias
+
+botonAgregarCategorias.onclick = () => {
+	const agregarCategorias = () => {
+		let agregarNuevasCategorias = inputAgregarCategorias.value;
+		const nuevasCategorias = {
+			id: 0,
+			nombre: agregarNuevasCategorias,
+		};
+		const funcionAuxiliar = guardarEnLocalStorage();
+		funcionAuxiliar.categorias.push(nuevasCategorias);
+		localStorage.setItem("tp-ahorradas", JSON.stringify(funcionAuxiliar));
+	};
+	agregarCategorias();
+};
 
 // boton reporte
 
 botonReporte.onclick = () => {
-    seccionReporte.classList.remove("is-hidden")
-    seccionPrincipal.classList.add("is-hidden")
-    seccionCategoria.classList.add("is-hidden")
-    seccionNuevaOperacion.classList.add("is-hidden")
-}
+	seccionReporte.classList.remove("is-hidden");
+	seccionPrincipal.classList.add("is-hidden");
+	seccionCategoria.classList.add("is-hidden");
+	seccionNuevaOperacion.classList.add("is-hidden");
+};
 
 //boton ocultar filtros SECCION FILTRO
 
 botonOcultarFiltros.onclick = () => {
-    divFormularioFiltros.classList.toggle("is-hidden")
-}
+	divFormularioFiltros.classList.toggle("is-hidden");
+};
 
-// boton nueva operacion SECCION OPERACIONES 
-
+// boton nueva operacion SECCION OPERACIONES
 
 botonNuevaOperacion.onclick = () => {
-    seccionNuevaOperacion.classList.remove("is-hidden")
-    seccionPrincipal.classList.add("is-hidden")
+	seccionNuevaOperacion.classList.remove("is-hidden");
+	seccionPrincipal.classList.add("is-hidden");
+};
 
-}  
-
-
-
+//guardar en local storage
+const guardarEnLocalStorage = () => {
+	let infoTraidaDeStorage = JSON.parse(localStorage.getItem("tp-ahorradas"));
+	if (!infoTraidaDeStorage) {
+		infoTraidaDeStorage = {
+			categorias: [
+				{
+					id: 1,
+					nombre: "Comidas",
+				},
+				{
+					id: 2,
+					nombre: "Servicios",
+				},
+				{
+					id: 3,
+					nombre: "Salidas",
+				},
+				{
+					id: 4,
+					nombre: "Educación",
+				},
+				{
+					id: 5,
+					nombre: "Transporte",
+				},
+				{
+					id: 6,
+					nombre: "Trabajo",
+				},
+			],
+			operaciones: [],
+		};
+	}
+	return infoTraidaDeStorage;
+};
